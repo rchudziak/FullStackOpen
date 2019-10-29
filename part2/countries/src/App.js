e03b732c47dab3b69dc451783c7b7e5711a353a0
Radek Chudziak <Radek.Chudziak@kcadeutag.com>
2019-10-29 10:04:17 +0000

    Exercise 6 - Countries - step 1

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -4,27 +4,58 @@ import axios from 'axios'
 
 const App = () => {
   const [countries, setCountries] = useState([])
-  const [countriesToShow, setCountriesToShow] = useState([])
+  const [filter, setFilter] = useState([])
 
   useEffect(() => {
     axios
       .get('https://restcountries.eu/rest/v2/all')
       .then(response => {
+        console.log(response.data)
         setCountries(response.data);
       })
   }, [])
 
+  const showLanguages = (countrylanguages) => {
+    return (
+      <ul>
+        {countrylanguages.map(language => <li>{language.name}</li>)}
+      </ul>
+    )
+  }
 
-const searchCountry = (event) =>{
+  const showCountries = () => {
 
-}
+    if (filter.length > 0) {
+      const countriesToShow = countries.filter(country => country.name.includes(filter))
 
+      if (countriesToShow.length === 1) {
+        const countryToShow = countriesToShow[0];
+        return (<div>
+          <h1>{countryToShow.name}</h1>
+          <div>Capital: {countryToShow.capital}</div>
+          <div>Population: {countryToShow.population}</div>
+          <h1>languages</h1>
+          {showLanguages(countryToShow.languages)}
+          <img src={countryToShow.flag} width="200" height="200"></img>
+        </div>
+        )
+      }
+
+      if (countriesToShow.length <= 10) {
+        return countriesToShow.map(country => <div key={country.alpha2Code}>{country.name}</div>)
+      } else {
+        return <div>Too many matches, specify another filter</div>
+      }
+
+    }
+
+  }
 
 
   return (
     <div >
-      find countries <input onChange={searchCountry} />
-      <div></div>
+      find countries <input onChange={(event) => setFilter(event.target.value)} />
+      <div>{showCountries()}</div>
     </div>
   );
 }
